0a1860973124bb373750b69192c7280c6420c38e
Ducko <micasniebuhr@gmail.com>
2023-06-28 21:55:26 +0200

    💎
    Removed sharding, cooldown and some other stuff due to this being a TEMPLATE:)

---
+++ src/events/client/ready.ts
@@ -0,0 +1,12 @@
+import { DiscordClient } from '../../bot.js';
+import { EventInterface } from '../../typings/index.js';
+import { Events } from 'discord.js';
+
+const event: EventInterface = {
+    name: Events.ClientReady,
+    options: { once: true, rest: false },
+    execute: async (client: DiscordClient) => {
+        console.log(`Logged in as ${client.user?.username}`)
+    },
+};
+export default event;
---
+++ src/events/interactionCreate/command.ts
@@ -0,0 +1,34 @@
+import { DiscordClient } from '../../bot.js';
+import { CommandInterface, EventInterface } from '../../typings/index';
+import { Events, ChatInputCommandInteraction } from 'discord.js';
+
+const event: EventInterface = {
+    name: Events.InteractionCreate,
+    options: { once: false, rest: false },
+    execute: async (interaction: ChatInputCommandInteraction, client: DiscordClient) => {
+        if (!interaction.isChatInputCommand()) return;
+        const command: CommandInterface | undefined = client.commands.get(interaction.commandName);
+        if (!command) {
+            return interaction.reply({
+                content: `${client.config.emojis.error} oh no! And error occured :(`,
+                ephemeral: true,
+            });
+        }
+
+        const subcommand = interaction.options.getSubcommand(false);
+        try {
+            if (subcommand) {
+                const subCommandFile = client.subcommands.get(`${interaction.commandName}.${subcommand}`);
+                subCommandFile?.execute(interaction, client);
+            } else {
+                command.execute(interaction, client);
+            }
+        } catch (error) {
+            return interaction.reply({
+                content: `${client.config.emojis.error} oh no! And error occured :(`,
+                ephemeral: true,
+            });
+        }
+    },
+};
+export default event;
