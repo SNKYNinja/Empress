ca3e5dbcbbbd65e9a4ad0811aed8356ff394bafb
Ninja <sharmila.chaudhary31@gmail.com>
2023-07-29 13:03:09 +0530

    ref: Remove ora code

--- src/functions/poru/nodeConnect.ts
+++ src/functions/poru/nodeConnect.ts
@@ -1,12 +1,6 @@
 import { Node } from 'poru';
-import ora from 'ora';
 import chalk from 'chalk';
 
 export const nodeConnect = async (node: Node) => {
     console.log(chalk.redBright.bold(`[NODE]`) + ` ${node.name} has been loaded successfully`);
-    // const spinner = ora(`Connecting to ${node.name}`).start();
-    // setTimeout(() => {
-    //     spinner.color = 'yellow';
-    //     spinner.text = `${chalk.bold.yellow('[NODE]')} Connected to ${node.name}`;
-    // }, 10 * 1000);
 };
