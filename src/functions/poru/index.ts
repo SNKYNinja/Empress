fad0c5afa34777a45df1b8d775333d55af88f381
Ninja <sharmila.chaudhary31@gmail.com>
2023-08-03 20:26:27 +0530

    fix: Missing nodeError export in index file

--- src/functions/poru/index.ts
+++ src/functions/poru/index.ts
@@ -1,3 +1,5 @@
+export { nodeError } from './nodeError.js';
 export { nodeConnect } from './nodeConnect.js';
 export { trackStart } from './trackStart.js';
 export { trackEnd } from './trackEnd.js';
+export { queueEnd } from './queueEnd.js';
