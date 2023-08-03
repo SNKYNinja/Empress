d29252e0e9381238b6a1ffb59e9c124f37e6ec38
Ninja <sharmila.chaudhary31@gmail.com>
2023-08-03 20:30:58 +0530

    style: Better nodeConnection event logs

--- src/functions/poru/nodeConnect.ts
+++ src/functions/poru/nodeConnect.ts
@@ -1,6 +1,18 @@
 import { Node } from 'poru';
 import chalk from 'chalk';
 
-export const nodeConnect = async (node: Node) => {
-    console.log(chalk.redBright.bold(`[NODE]`) + ` ${node.name} has been loaded successfully`);
+export const nodeConnect = async (node: Node, elapsedTime: number) => {
+    const lightGreen = '#09bb74';
+
+    const darkGreen = '##045c3b';
+    const darkText = '##696869';
+
+    const lightArrow = chalk.bold.hex(lightGreen)('➜');
+    const darkArrow = chalk.bold.hex(darkGreen)('➜');
+    const nodeName = node.name + ':';
+
+    elapsedTime = (Date.now() - elapsedTime) / 1000;
+
+    console.log(`   ${lightArrow} ${chalk.bold.underline(nodeName)} ${chalk.bold.hex(lightGreen)(node.restURL)}`);
+    console.log(`   ${darkArrow} ${chalk.bold.hex(darkText)('Ready in')} ${elapsedTime} sec\n`);
 };
