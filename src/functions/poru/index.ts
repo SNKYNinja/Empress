f7684588d955fe97a2cd628823712666a797b982
Ninja <96983394+SNKYNinja@users.noreply.github.com>
2023-09-07 21:38:35 +0530

    Delete src/functions directory

--- src/functions/poru/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './nodeError.js';
-export * from './nodeConnect.js';
-export * from './trackStart.js';
-export * from './trackEnd.js';
-export * from './queueEnd.js';
-export * from './playerDestroy.js';
--- src/functions/poru/trackEnd.ts
+++
@@ -1,6 +0,0 @@
-import { Player, Track } from 'poru';
-import { DiscordClient } from 'bot.js';
-
-export const trackEnd = (player: Player | any, track: Track, client: DiscordClient) => {
-    return player.message?.delete().catch(() => {});
-};
